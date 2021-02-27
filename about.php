5e7d67b5bbd11610a301e757fca6ee6279b54fed
uryevich <uryevich@gmail.com>
2021-02-27 20:22:01 +0300

    add gitignore

--- about.php
+++ about.php
@@ -1,5 +1,5 @@
 <?php
-require 'db_ini.php';
+// require 'db_ini.php';
 
 // $query="SELECT * FROM glrs WHERE trip=0";
 // $result=mysql_query($query, $dbid);
@@ -9,21 +9,22 @@ require 'db_ini.php';
 // $result=mysql_query($query, $dbid);
 // $trips=mysql_num_rows($result);
 
-$query="SELECT * FROM glrs";
-$result=mysql_query($query, $dbid);
-$objects=mysql_num_rows($result);
+// $query="SELECT * FROM glrs";
+// $result=mysql_query($query, $dbid);
+// $objects=mysql_num_rows($result);
 
-$query="SELECT * FROM files";
-$result=mysql_query($query, $dbid);
-$photos=mysql_num_rows($result);
+// $query="SELECT * FROM files";
+// $result=mysql_query($query, $dbid);
+// $photos=mysql_num_rows($result);
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
-<HTML>
+<!DOCTYPE html>
+<html>
 <HEAD>
 <title>Abandoned - About</title>
- <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1251">
- <META NAME="Keywords" CONTENT="industrial, places, urban, abandoned, plant, base, zone, завод, зона, место, брошенные, заброшенные, недостроенный, база">
- <META NAME="Description" CONTENT="Photo base of abandonned plants, unfinished buildings, industrial sites">
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="adventure, industrial, places, urban, abandoned, plant, base, zone, uryevich, завод, зона, место, брошенные, заброшенные, недостроенный, база">
+ <meta name="Description" content="Photo base of abandonned plants, unfinished buildings, industrial sites">
 <style type=text/css>
 body {
 background-color: #000017;
@@ -39,22 +40,11 @@ hr { border: 1px solid #747687; }
 .utext {  font-family: Arial; font-size: 8pt; color: #a4b6c7; font-style: italic; text-decoration: none}
 .link {  font-family: Verdana, Arial; font-size: 9pt; font-weight: bold}
 </style>
-<!-- G+ -->
-<link href="https://plus.google.com/107941468976926491069" rel="publisher" />
-<!-- G+ -->
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-15607830-1']);
-  _gaq.push(['_trackPageview']);
+<?php require ('gtag.js') ?>
+<?php require ('adSence.js') ?>
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-</HEAD>
+</head>
 <body text="#FFFFFF" link="#00b0f0" vlink="#00b0d0" alink="#ff2040">
 <?php require 'menu.php'; ?>
 
@@ -72,45 +62,13 @@ Who I am?<br><br>
 &nbsp;&nbsp;&nbsp;I think we are all not indifferent to abandoned things. The Abandoned have some sort of a strong and complicated connection with our souls; some people get scared and try to escape their impressions, some fight with them and try to destroy or rebuild or just leave their own footprint on the abandoned site to prove that they're stronger than this world. And some do not try to do anything - they just look and listen to the Abandoned, enjoying those impressions, feeling the real meaning of time. I am one of them.
 <p>
 &nbsp;&nbsp;&nbsp;If you have something to say about this site, your site or whatever, you can write me a <a href="mailto:uryevich@gmail.com?subject=www.abandoned.ru">letter</a>.
-<p align=right class=text>
-At the moment 'Abandoned' contains <i><?php echo $photos ?> photos</i> in <i><?php echo $objects ?> galeries</i>.<br>
-<br>&nbsp;&nbsp;&nbsp;&nbsp;
-<!-- G+ -->
-<!-- Place this tag where you want the badge to render -->
-<a href="https://plus.google.com/107941468976926491069?prsrc=3" style="text-decoration:none;"><img src="https://ssl.gstatic.com/images/icons/gplus-16.png" alt="" style="border:0;width:16px;height:16px;"/></a>
-<!-- G+ -->
-&nbsp;&nbsp;&nbsp;&nbsp;
-</p>
 
 </td>
 </tr>
 </table>
 <?php require ('copyright.php'); ?>
 <hr>
-<div align="center">
-<!-- google_ad_start -->
-<script type="text/javascript"><!--
-google_ad_client = "pub-9659481331039989";
-/* 468x60, created 4/1/10 */
-google_ad_slot = "6602157302";
-google_ad_width = 468;
-google_ad_height = 60;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-<!-- google_ad_end -->
-</div>
+
 <div align=right><?php include 'hotlog.php'; ?></div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-15607830-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
-</BODY>
-</HTML>
+</body>
+</html>
--- thanx.php
+++ thanx.php
@@ -1,10 +1,11 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
-<HTML>
-<HEAD>
+<!DOCTYPE html>
+<html>
+<head>
 <title>Abandoned - Thanks</title>
- <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1251">
- <META NAME="Keywords" CONTENT="industrial, places, urban, abandoned, plant, base, zone, завод, зона, место, брошенные, заброшенные, недостроенный, база">
- <META NAME="Description" CONTENT="Photo base of abandonned plants, unfinished buildings, industrial sites">
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="keywords" content="adventure, industrial, places, urban, abandoned, plant, base, zone, завод, зона, место, брошенные, заброшенные, недостроенный, база">
+ <meta NAME="Description" CONTENT="Photo base of abandonned plants, unfinished buildings, industrial sites">
 <style type=text/css>
 body {
 background-color: #000017;
@@ -20,26 +21,17 @@ hr { border: 1px solid #747687; }
 .utext {  font-family: Arial; font-size: 8pt; color: #a4b6c7; font-style: italic; text-decoration: none}
 .link {  font-family: Verdana, Arial; font-size: 9pt; font-weight: bold}
 </style>
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-15607830-1']);
-  _gaq.push(['_trackPageview']);
+<?php require ('gtag.js') ?>
+<?php require ('adSence.js') ?>
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-</HEAD>
+</head>
 <body text="#FFFFFF" link="#00b0f0" vlink="#00b0d0" alink="#ff2040">
 <?php require ('menu.php'); ?>
 <p align="right" class="stext">Peoples who opened to me and continue charge me to explore <font color="#ffffff">INDUSTRIAL</font>.
 </p><hr width="40%" align="right">
 <p align="left" class="text"><font color="#ffffff">Timophey</font> - His energy always force us to go. 
-<a href="http://www.black-light.ru/urbantrip/index.php" target="_new" class="link">Link</a></p>
+<a href="http://www.urbantrip.ru" target="_new" class="link">Link</a></p>
 <p align="left" class="text"><font color="#ffffff">Volod</font> - the MAN who started all this and gather us under Urban Club.
 <a href="http://urban.cyberpunk.ru" target="_new" class="link">Link</a></p>
 <p align="left" class="text"><font color="#ffffff">NeoFiction</font> - she gives me a deep charge of Industrial by her storyes and ideas.
@@ -48,19 +40,10 @@ hr { border: 1px solid #747687; }
 <!-- <a href="http://deadcities.boom.ru" target="_new" class="link">Link</a> --></p>
 <p align="left" class="text"><font color="#ffffff">nix[on]</font> - He is a man, who makes just a good mood in each trip.
 <!-- <a href="http://cpt.nm.ru" target="_new" class="link">Link</a></p> -->
-<p align="right" class="text">And I appreciate very much peoples who gave information, help or support me to make this project.
+<p align="right" class="text">And I appreciate very much peoples who gave information, help or support me in Urban Exploration.
 <br>
 <?php require ('copyright.php'); ?>
 <hr>
 <div align="right"><?php include 'hotlog.php'; ?></div>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-15607830-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
 </BODY>
 </HTML>
